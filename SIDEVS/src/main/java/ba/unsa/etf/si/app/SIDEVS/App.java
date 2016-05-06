82dac2d3abfec7ee848f3a0129d149e71eeec8b1
Amar Basic <amarbasic4@gmail.com>
2016-05-06 15:29:48 +0200

    brisanje duplikata

--- SIDEVS/src/main/java/ba/unsa/etf/si/app/SIDEVS/App.java
+++ SIDEVS/src/main/java/ba/unsa/etf/si/app/SIDEVS/App.java
@@ -24,10 +24,6 @@ public class App {
 	
 	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException {
 		Session session = HibernateUtil.getSessionFactory().openSession();
-<<<<<<< HEAD
-		Login l = new Login();
-		l.main(args);
-=======
 	 	//kreirajSkladiste(session, 3);
 		try {
 			System.out.println("Kreiram!");
@@ -41,18 +37,15 @@ public class App {
 			k.setDatum_polaska_rada(new Date());
 			k.setRadno_mjesto("Radnik");
 			k.setLozinka("password");
-			kreirajKorisnikaRadnik(session, k);
+			//kreirajKorisnikaRadnik(session, k);
 			System.out.println("Korisnik kreiran!");
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
 		//dajSveKorisnike(session);
->>>>>>> branch 'master' of https://github.com/SoftverInzenjeringETFSA/SI2015Tim1.git
 	}
-
-<<<<<<< HEAD
+	
 	/*private static void kreirajKorisnikaAdmin(Session session, Administrator a) {
-=======
 	private static void kreirajKorisnikaRadnik(Session session, Radnik a) {
 		Transaction t = session.beginTransaction();
 		session.save(a);
