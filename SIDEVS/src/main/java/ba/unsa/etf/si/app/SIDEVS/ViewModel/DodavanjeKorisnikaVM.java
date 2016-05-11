660d66b36d1ac785c7c315d21737e809d40ad3fa
Maki <Maki@DESKTOP-M89LLT2>
2016-05-11 21:10:09 +0200

    klasa za konverzije

--- SIDEVS/src/main/java/ba/unsa/etf/si/app/SIDEVS/ViewModel/DodavanjeKorisnikaVM.java
+++ SIDEVS/src/main/java/ba/unsa/etf/si/app/SIDEVS/ViewModel/DodavanjeKorisnikaVM.java
@@ -23,6 +23,7 @@ import ba.unsa.etf.si.app.SIDEVS.Model.Sessions;
 import ba.unsa.etf.si.app.SIDEVS.Model.Skladiste;
 import ba.unsa.etf.si.app.SIDEVS.Util.HibernateUtil;
 import ba.unsa.etf.si.app.SIDEVS.View.Login;
+import ba.unsa.etf.si.app.SIDEVS.Validation.Conversions;
 
 public class DodavanjeKorisnikaVM {
 
@@ -44,7 +45,7 @@ public class DodavanjeKorisnikaVM {
 			k.setAdresa(adresa);
 			k.setEmail(email);
 			k.setTelefon(brojTelefona);
-			k.setDatum_polaska_rada(convertDates(datumPocetkaRada)); 
+			k.setDatum_polaska_rada(Conversions.dajDatumZaBazu(datumPocetkaRada)); 
 			k.setRadno_mjesto(radnoMjesto);
 			k.setLozinka("password");
 			ses.getSession().save(k);
@@ -56,19 +57,7 @@ public class DodavanjeKorisnikaVM {
 		return true;		
 	}
 	
-	public static java.util.Date convertDates(String input) throws ParseException{
-		java.text.SimpleDateFormat sdf1 = 
-			     new java.text.SimpleDateFormat("dd.mm.yyyy");
-		
-		java.util.Date date = sdf1.parse(input);
-		
-		java.text.SimpleDateFormat sdf2 = 
-		     new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		
-		String s = sdf2.format(date);
-		return sdf2.parse(s);
-		
-	}
+	
 	
 
 }
