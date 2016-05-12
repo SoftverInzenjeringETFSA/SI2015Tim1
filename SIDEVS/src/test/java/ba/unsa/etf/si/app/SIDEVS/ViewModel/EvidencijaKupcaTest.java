bdea2753139e3585c3a50a5c333d526a5fc22708
adnela <acavcic1@etf.unsa.ba>
2016-05-12 14:09:33 -0700

    P

---
+++ SIDEVS/src/test/java/ba/unsa/etf/si/app/SIDEVS/ViewModel/EvidencijaKupcaTest.java
@@ -0,0 +1,39 @@
+package ba.unsa.etf.si.app.SIDEVS.ViewModel;
+
+import static org.junit.Assert.*;
+
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.junit.Test;
+
+import ba.unsa.etf.si.app.SIDEVS.Model.Kupac;
+import ba.unsa.etf.si.app.SIDEVS.Model.Sessions;
+
+public class EvidencijaKupcaTest {
+
+
+
+	@Test
+	public void DodajKupcaTest() {
+		try
+		{
+			Sessions s= new Sessions("admin","password");
+			EvidencijaKupca ek= new EvidencijaKupca(s);
+			String naziv="apoteka 1";
+			String adresa="adresica";
+		ek.dodajKupca(naziv, adresa);
+		Boolean rezultat= false;
+		if(s.getSession().createCriteria(Kupac.class).add(Restrictions.eq("naziv", naziv)).setProjection(Projections.property("naziv")).uniqueResult()!=null) 
+			rezultat= true;
+		assertTrue(rezultat);
+
+		
+		}
+		catch(Exception e)
+		{
+			fail("Test pao!");
+			
+		}
+
+}
+}
