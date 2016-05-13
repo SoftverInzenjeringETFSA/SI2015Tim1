f058e913367eeb2af9bf37cb7bc472d2fa5c6489
Amar Basic <amarbasic4@gmail.com>
2016-05-13 19:17:56 +0200

    blocker

--- SIDEVS/src/main/java/ba/unsa/etf/si/app/SIDEVS/Util/HibernateUtil.java
+++ SIDEVS/src/main/java/ba/unsa/etf/si/app/SIDEVS/Util/HibernateUtil.java
@@ -1,25 +1,26 @@
 package ba.unsa.etf.si.app.SIDEVS.Util;
 
+import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
-	  private static final SessionFactory sessionFactory = buildSessionFactory();
+	final static Logger logger = Logger.getLogger(HibernateUtil.class);
+	private static final SessionFactory sessionFactory = buildSessionFactory();
 
-	    private static SessionFactory buildSessionFactory() {
-	        try {
-	            // Create the SessionFactory from hibernate.cfg.xml
-	            return new Configuration().configure().buildSessionFactory();
-	        }
-	        catch (Throwable ex) {
-	            // Make sure you log the exception, as it might be swallowed
-	            System.err.println("Initial SessionFactory creation failed." + ex);
-	            throw new ExceptionInInitializerError(ex);
-	        }
-	    }
+	private static SessionFactory buildSessionFactory() {
+		try {
+			// Create the SessionFactory from hibernate.cfg.xml
+			return new Configuration().configure().buildSessionFactory();
+		} catch (Exception ex) {
+			// Make sure you log the exception, as it might be swallowed
+			logger.error(ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
 
-	    public static SessionFactory getSessionFactory() {
-	        return sessionFactory;
-	    }
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
 }
