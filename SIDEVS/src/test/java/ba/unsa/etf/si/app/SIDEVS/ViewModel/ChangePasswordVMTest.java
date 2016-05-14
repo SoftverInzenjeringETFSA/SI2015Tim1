c17c35b14d3fa1b106de60c45482ee028bb9ce38
Maki <Maki@DESKTOP-M89LLT2>
2016-05-14 08:27:40 +0200

    Provjera

--- SIDEVS/src/test/java/ba/unsa/etf/si/app/SIDEVS/ViewModel/ChangePasswordVMTest.java
+++ SIDEVS/src/test/java/ba/unsa/etf/si/app/SIDEVS/ViewModel/ChangePasswordVMTest.java
@@ -1,6 +1,10 @@
 package ba.unsa.etf.si.app.SIDEVS.ViewModel;
 
 import static org.junit.Assert.*;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import ba.unsa.etf.si.app.SIDEVS.Model.*;
@@ -9,19 +13,15 @@ import org.junit.Test;
 public class ChangePasswordVMTest {
 
 	@Test
-	public void ChangePasswordTest() {
-		try
-		{
+	public void ChangePasswordTest() throws NoSuchAlgorithmException, InvalidKeySpecException {
+
 			char[] newPassword= "newPassword".toCharArray();
 			char[] oldPassword="password".toCharArray();
 			
 			assertTrue(ba.unsa.etf.si.app.SIDEVS.ViewModel.ChangePasswordVM.ChangePassword("admin",oldPassword,newPassword,newPassword));
 			assertTrue(ba.unsa.etf.si.app.SIDEVS.ViewModel.ChangePasswordVM.ChangePassword("admin", newPassword, oldPassword, oldPassword));
-		}
-		catch (Exception ex)
-		{
-			fail("Bacen izuzetak!");
-		}
+		
+
 	}
 
 }
