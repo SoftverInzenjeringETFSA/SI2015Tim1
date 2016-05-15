c9f33cca69dd00df21df544991741eb41194804f
Maki <Maki@DESKTOP-M89LLT2>
2016-05-15 11:10:23 +0200

    Automatski generisan ID za lijek

--- SIDEVS/src/main/java/ba/unsa/etf/si/app/SIDEVS/Model/Lijek.java
+++ SIDEVS/src/main/java/ba/unsa/etf/si/app/SIDEVS/Model/Lijek.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -11,14 +13,11 @@ public class Lijek implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	
 	@Id
-	private Long id;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
 	public String getNaziv() {
 		return naziv;
 	}
