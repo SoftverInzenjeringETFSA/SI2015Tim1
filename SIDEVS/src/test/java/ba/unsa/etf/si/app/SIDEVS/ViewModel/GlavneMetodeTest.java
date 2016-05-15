6d416b9a2d15edce6fa9a5ff1787df372a4cf41b
Maki <Maki@DESKTOP-M89LLT2>
2016-05-15 17:19:47 +0200

    unit testovi

--- SIDEVS/src/test/java/ba/unsa/etf/si/app/SIDEVS/ViewModel/GlavneMetodeTest.java
+++ SIDEVS/src/test/java/ba/unsa/etf/si/app/SIDEVS/ViewModel/GlavneMetodeTest.java
@@ -2,11 +2,14 @@ package ba.unsa.etf.si.app.SIDEVS.ViewModel;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.junit.Test;
 
@@ -21,65 +24,261 @@ public class GlavneMetodeTest {
 	@Test
 	public void vratiSkladistaTest(){
 		try{
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		Transaction trans = session.beginTransaction();
-		Administrator k = null;
-		//kreiranje administratora
-			k = new Administrator();
-			k.setIme("adminTest");
-			k.setPrezime("Korisnicki");
-			k.setJmbg("1234567891234");
-			k.setAdresa("Adresa bb");
-			k.setEmail("adminTest");
-			k.setTelefon("012353451");
-			k.setDatum_polaska_rada(new Date());
-			k.setRadno_mjesto("radnik");
-			k.setLozinka("password");
-			
+			Session session = HibernateUtil.getSessionFactory().openSession();
+			Transaction trans = session.beginTransaction();
+			Administrator k = null;
+				k = new Administrator();
+				k.setIme("adminTest");
+				k.setPrezime("Korisnicki");
+				k.setJmbg("1234567891234");
+				k.setAdresa("Adresa bb");
+				k.setEmail("adminTest");
+				k.setTelefon("012353451");
+				k.setDatum_polaska_rada(new Date());
+				k.setRadno_mjesto("radnik");
+				k.setLozinka("password");			
 			session.save(k);
 			trans.commit();
-		
-
-		Sessions s = new Sessions ("adminTest","password");
-		
-		
-		Transaction t = s.getSession().beginTransaction();
-		Skladiste skladiste = new Skladiste();
-		GlavneMetode g = new GlavneMetode();
-		LotVM l = null;
-		LijekVM lk;
-
-			l = new LotVM(s);
-			lk = new LijekVM(s);
-			LijekVM.dodajLijek("lijekTest", "fr");	
 			
-
+			Sessions s = new Sessions ("adminTest","password");
+			
+			
+			Transaction t = s.getSession().beginTransaction();
+				Skladiste skladiste = new Skladiste();
+				skladiste.setBroj_skladista(20);
+				LijekVM.dodajLijek("lijekTest", "fr");	
+				s.getSession().save(skladiste);
+			t.commit();
+			
+			Lijek lijek = (Lijek) s.getSession().createCriteria(Lijek.class).add(Restrictions.eq("naziv", "lijekTest")).list().get(0);
+			LotVM.dodajLot("555666", (Double)10.5, (Double)10.5, Conversions.stringToDate("12.12.2020"), 12, lijek, skladiste, 10);
+			Lot lot = (Lot)s.getSession().createCriteria(Lot.class).add(Restrictions.eq("broj_lota", "555666")).list().get(0);
+			
+			Set<Skladiste> skladista = GlavneMetode.vratiSkladista(lot);
 		
-		
-		skladiste.setBroj_skladista(20);
-		s.getSession().save(skladiste);
-		t.commit();
-		
-		Lijek lijek = (Lijek) s.getSession().createCriteria(Lijek.class).add(Restrictions.eq("naziv", "lijekTest")).list().get(0);
-		LotVM.dodajLot("555666", (Double)10.5, (Double)10.5, Conversions.stringToDate("12.12.2020"), 12, lijek, skladiste, 10);
-
-		Lot lot = (Lot)s.getSession().createCriteria(Lot.class).add(Restrictions.eq("broj_lota", "555666")).list().get(0);
-		System.out.println(lot.getPakovanja().size());
-		Set<Skladiste> skladista = g.vratiSkladista(lot);
-	
-		
-		assertTrue(skladista.contains(skladiste));
-		s.getSession().delete(skladiste);
-		s.getSession().delete(lijek);
-		s.getSession().delete(lot);
-		t.commit();
-		
-		session.delete(k);
-		trans.commit();
-		
-	} catch (Exception ex) {
-		System.out.println(ex);
+			assertTrue(skladista.contains(skladiste));
+			
+			s.getSession().delete(skladiste);
+			s.getSession().delete(lijek);
+			s.getSession().delete(lot);
+			t.commit();
+			
+			session.delete(k);
+			trans.commit();
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}	
 	}
 	
+	@Test
+	public void vratiSveLotoveTest(){
+		try{
+			Session session = HibernateUtil.getSessionFactory().openSession();
+			Transaction trans = session.beginTransaction();
+			Administrator k = null;
+				k = new Administrator();
+				k.setIme("adminTest");
+				k.setPrezime("Korisnicki");
+				k.setJmbg("1234567891234");
+				k.setAdresa("Adresa bb");
+				k.setEmail("adminTest");
+				k.setTelefon("012353451");
+				k.setDatum_polaska_rada(new Date());
+				k.setRadno_mjesto("radnik");
+				k.setLozinka("password");			
+			session.save(k);
+			trans.commit();
+			
+			Sessions s = new Sessions ("adminTest","password");
+			
+			
+			Transaction t = s.getSession().beginTransaction();
+				Skladiste skladiste = new Skladiste();
+				skladiste.setBroj_skladista(20);
+				LijekVM.dodajLijek("lijekTest", "fr");	
+				s.getSession().save(skladiste);
+			t.commit();
+			
+			Lijek lijek = (Lijek) s.getSession().createCriteria(Lijek.class).add(Restrictions.eq("naziv", "lijekTest")).list().get(0);
+			LotVM.dodajLot("555666", (Double)10.5, (Double)10.5, Conversions.stringToDate("12.12.2020"), 12, lijek, skladiste, 10);
+			Lot lot = (Lot)s.getSession().createCriteria(Lot.class).add(Restrictions.eq("broj_lota", "555666")).list().get(0);
+		
+			assertTrue(GlavneMetode.vratiSveLotove(s).contains(lot));
+			
+			s.getSession().delete(skladiste);
+			s.getSession().delete(lijek);
+			s.getSession().delete(lot);
+			t.commit();
+			
+			session.delete(k);
+			trans.commit();
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}	
 	}
+	
+	@Test
+	public void vratiOtpisaneLotoveTest(){
+		try{
+			
+			Session session = HibernateUtil.getSessionFactory().openSession();
+			Transaction trans = session.beginTransaction();
+			Administrator k = null;
+				k = new Administrator();
+				k.setIme("adminTest");
+				k.setPrezime("Korisnicki");
+				k.setJmbg("1234567891234");
+				k.setAdresa("Adresa bb");
+				k.setEmail("adminTest");
+				k.setTelefon("012353451");
+				k.setDatum_polaska_rada(new Date());
+				k.setRadno_mjesto("radnik");
+				k.setLozinka("password");			
+			session.save(k);
+			trans.commit();
+			
+			Sessions s = new Sessions ("adminTest","password");
+			
+			
+			Transaction t = s.getSession().beginTransaction();
+				Skladiste skladiste = new Skladiste();
+				skladiste.setBroj_skladista(20);
+				LijekVM.dodajLijek("lijekTest", "fr");	
+				s.getSession().save(skladiste);
+			t.commit();
+			
+			Lijek lijek = (Lijek) s.getSession().createCriteria(Lijek.class).add(Restrictions.eq("naziv", "lijekTest")).list().get(0);
+			LotVM.dodajLot("555666", (Double)10.5, (Double)10.5, Conversions.stringToDate("12.12.2020"), 12, lijek, skladiste, 10);
+			Lot lot = (Lot)s.getSession().createCriteria(Lot.class).add(Restrictions.eq("broj_lota", "555666")).list().get(0);
+		
+			OtpisLijekovaVM.otpisiLijek("555666");
+			
+			List<Lot> lotovi = GlavneMetode.vratiOtpisaneLotove(s);
+			
+			assertTrue(lotovi.contains(lot));
+			
+			s.getSession().delete(skladiste);
+			s.getSession().delete(lijek);
+			s.getSession().delete(lot);
+			t.commit();
+			Sessions.ubijSesiju();
+			session.delete(k);
+			trans.commit();
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}	
+	}
+		
+		@Test
+		public void vratiNeotpisaneLotoveTest(){
+			try{
+				
+				Session session = HibernateUtil.getSessionFactory().openSession();
+				Transaction trans = session.beginTransaction();
+				Administrator k = null;
+					k = new Administrator();
+					k.setIme("adminTest");
+					k.setPrezime("Korisnicki");
+					k.setJmbg("1234567891234");
+					k.setAdresa("Adresa bb");
+					k.setEmail("adminTest");
+					k.setTelefon("012353451");
+					k.setDatum_polaska_rada(new Date());
+					k.setRadno_mjesto("radnik");
+					k.setLozinka("password");			
+				session.save(k);
+				trans.commit();
+				
+				Sessions s = new Sessions ("adminTest","password");
+				
+				
+				Transaction t = s.getSession().beginTransaction();
+					Skladiste skladiste = new Skladiste();
+					skladiste.setBroj_skladista(20);
+					LijekVM.dodajLijek("lijekTest", "fr");	
+					s.getSession().save(skladiste);
+				t.commit();
+				
+				Lijek lijek = (Lijek) s.getSession().createCriteria(Lijek.class).add(Restrictions.eq("naziv", "lijekTest")).list().get(0);
+				LotVM.dodajLot("555666", (Double)10.5, (Double)10.5, Conversions.stringToDate("12.12.2020"), 12, lijek, skladiste, 10);
+				Lot lot = (Lot)s.getSession().createCriteria(Lot.class).add(Restrictions.eq("broj_lota", "555666")).list().get(0);
+				
+				List<Lot> lotovi = GlavneMetode.vratiNeotpisaneLotove(s);
+				
+				assertTrue(lotovi.contains(lot));
+				
+				s.getSession().delete(skladiste);
+				s.getSession().delete(lijek);
+				s.getSession().delete(lot);
+				t.commit();
+				Sessions.ubijSesiju();
+				session.delete(k);
+				trans.commit();
+				} catch (Exception ex) {
+				System.out.println(ex);
+			}	
+		
+		}
+	
+		@Test
+		public void vratiSveLotoveOdDoTest(){
+			try{
+				
+				Session session = HibernateUtil.getSessionFactory().openSession();
+				Transaction trans = session.beginTransaction();
+				Administrator k = null;
+					k = new Administrator();
+					k.setIme("adminTest");
+					k.setPrezime("Korisnicki");
+					k.setJmbg("1234567891234");
+					k.setAdresa("Adresa bb");
+					k.setEmail("adminTest");
+					k.setTelefon("012353451");
+					k.setDatum_polaska_rada(new Date());
+					k.setRadno_mjesto("radnik");
+					k.setLozinka("password");			
+				session.save(k);
+				trans.commit();
+				
+				Sessions s = new Sessions ("adminTest","password");
+				
+				
+				Transaction t = s.getSession().beginTransaction();
+					Skladiste skladiste = new Skladiste();
+					skladiste.setBroj_skladista(20);
+					LijekVM.dodajLijek("lijekTest", "fr");	
+					s.getSession().save(skladiste);
+				t.commit();
+				
+				Lijek lijek = (Lijek) s.getSession().createCriteria(Lijek.class).add(Restrictions.eq("naziv", "lijekTest")).list().get(0);
+				LotVM.dodajLot("555666", (Double)10.5, (Double)10.5, Conversions.stringToDate("12.12.2020"), 12, lijek, skladiste, 10);
+				Lot lot = (Lot)s.getSession().createCriteria(Lot.class).add(Restrictions.eq("broj_lota", "555666")).list().get(0);
+				
+				Calendar cal = Calendar.getInstance();
+				cal.add(Calendar.DATE, -7);
+				Date d1 = cal.getTime();
+				Calendar cal2 = Calendar.getInstance();
+				cal2.add(Calendar.DATE, +7);
+				Date d2 = cal2.getTime();
+				
+				List<Lot> lotovi = GlavneMetode.vratiSveLotoveOdDo(s, Conversions.dateToString(d1), Conversions.dateToString(d2));
+				
+				assertTrue(lotovi.contains(lot));
+				
+				s.getSession().delete(skladiste);
+				s.getSession().delete(lijek);
+				s.getSession().delete(lot);
+				t.commit();
+				Sessions.ubijSesiju();
+				session.delete(k);
+				trans.commit();
+			} catch (Exception ex) {
+				System.out.println(ex);
+			}	
+		
+	}
+	
+	
+	
+	
 }
