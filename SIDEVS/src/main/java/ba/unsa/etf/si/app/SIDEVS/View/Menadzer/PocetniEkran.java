1e7d277a3628527c8f6215c89d8d5c26ce9173f6
Amar Basic <amarbasic4@gmail.com>
2016-05-19 16:47:13 +0200

    vidljivost dodaj/azuriraj

--- SIDEVS/src/main/java/ba/unsa/etf/si/app/SIDEVS/View/Menadzer/PocetniEkran.java
+++ SIDEVS/src/main/java/ba/unsa/etf/si/app/SIDEVS/View/Menadzer/PocetniEkran.java
@@ -78,7 +78,7 @@ public class PocetniEkran {
 		frmMenadzerPocetniEkran = new JFrame();
 		frmMenadzerPocetniEkran.setResizable(false);
 		frmMenadzerPocetniEkran.setTitle("Početni ekran");
-		frmMenadzerPocetniEkran.setBounds(100, 100, 695, 300);
+		frmMenadzerPocetniEkran.setBounds(100, 100, 731, 300);
 		frmMenadzerPocetniEkran.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		frmMenadzerPocetniEkran.getContentPane().setLayout(null);
 		frmMenadzerPocetniEkran.setLocationRelativeTo(null);
@@ -156,7 +156,7 @@ public class PocetniEkran {
 				}
 			}
 		});
-		btnPretragaLijekova.setBounds(528, 28, 141, 23);
+		btnPretragaLijekova.setBounds(528, 28, 178, 23);
 		frmMenadzerPocetniEkran.getContentPane().add(btnPretragaLijekova);
 		
 		JButton btnKreiranjeFakture = new JButton("Kreiranje fakture");
@@ -188,7 +188,7 @@ public class PocetniEkran {
 				}
 			}
 		});
-		btnOtpisLijekova.setBounds(528, 62, 141, 23);
+		btnOtpisLijekova.setBounds(528, 62, 178, 23);
 		frmMenadzerPocetniEkran.getContentPane().add(btnOtpisLijekova);
 		
 		JButton btnDesavanjaZaOdredjeni = new JButton("Dešavanja za određeni vremenski period");
@@ -248,7 +248,7 @@ public class PocetniEkran {
 			}
 		});
 
-		btnObrisi.setBounds(528, 160, 141, 23);
+		btnObrisi.setBounds(528, 160, 178, 23);
 		frmMenadzerPocetniEkran.getContentPane().add(btnObrisi);
 		
 		JButton btnOdjava = new JButton("Odjava");
@@ -298,7 +298,7 @@ public class PocetniEkran {
             JOptionPane.showMessageDialog(null, help);
 			}
 		});
-		btnPomoc.setBounds(528, 228, 141, 23);
+		btnPomoc.setBounds(528, 228, 178, 23);
 		frmMenadzerPocetniEkran.getContentPane().add(btnPomoc);
 		
 		JButton btnDodaj = new JButton("Dodaj");
@@ -347,7 +347,7 @@ public class PocetniEkran {
 					}
 			}
 		});
-		btnDodajLotU.setBounds(528, 96, 141, 23);
+		btnDodajLotU.setBounds(528, 96, 178, 23);
 		frmMenadzerPocetniEkran.getContentPane().add(btnDodajLotU);
 	}
 }
